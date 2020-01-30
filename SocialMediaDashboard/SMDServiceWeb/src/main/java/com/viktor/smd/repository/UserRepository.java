5f9ec15be0874f4c706d4323cf84d51204845027
Viktor Zdravkovski <viktorzdravkovski995@gmail.com>
2020-01-30 20:03:15 +0100

    Add files via upload

---
+++ SocialMediaDashboard/SMDServiceWeb/src/main/java/com/viktor/smd/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.viktor.smd.repository;
+
+import com.viktor.smd.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}

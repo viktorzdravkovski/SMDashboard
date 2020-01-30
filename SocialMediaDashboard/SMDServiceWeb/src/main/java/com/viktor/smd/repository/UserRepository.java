87c1fc367335399805cf61a8079e86ea333944d1
Viktor Zdravkovski <viktorzdravkovski995@gmail.com>
2020-01-30 20:06:36 +0100

    Deleted duplicate folder.

--- SocialMediaDashboard/SMDServiceWeb/src/main/java/com/viktor/smd/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.viktor.smd.repository;
-
-import com.viktor.smd.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
